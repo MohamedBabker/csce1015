379b34398d9b9ee6f290adb832373a8e444fc0b5
Babiker, Mohamed <mohamedbabiker@my.unt.edu>
2024-09-22 18:36:04 -0500

    Updated name and fixed card length check

--- 1015-WWCCV.py
+++ 1015-WWCCV.py
@@ -4,7 +4,7 @@
 # anything that starts with a pound sign (hashtag if you are under 30) is a comment. The computer ignores these.
 
 # Let's tell the world whose awful credit card validator this is.
-print ("Joe Dirt Modified This Credit Card Validator")
+print ("Mohamed Babiker Modified This Credit Card Validator")
 
 # Let's get the card number from the user
 card_number = input("Enter your 16-digit credit card number: ")
@@ -14,7 +14,7 @@ card_number = input("Enter your 16-digit credit card number: ")
 # len means 'length', and the '==' is testing one thing against the other. A single equal sign would set one thing equal to the other (not what we want to do). 
 # card_number.isdigit() is checking if this is numbers instead of someone typing 'cheeseburger' for their credit card number.
 
-if len(card_number) == 15 and card_number.isdigit():     
-    print ("Card is valid.")
+if len(card_number) == 16:  # Checks if card number is 16 digits
+    print("Valid card number.")
 else:
-    print ("Invalid card number. It must be 16 digits long.") 
+    print("Invalid card number. Please enter a 16-digit number.")
